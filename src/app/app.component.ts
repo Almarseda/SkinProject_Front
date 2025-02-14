c2d9101e7abfcfa48ae791bfd0be9189e02f33c2
DatSatoru <almarseda@gmail.com>
2025-02-14 13:36:37 +0100

    sidebar created and added, need some fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardProductComponent } from "./components/card-product/card-product.component";
 import { ProductsComponent } from "./components/products/products.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardProductComponent, ProductsComponent],
+  imports: [RouterOutlet, ProductsComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
