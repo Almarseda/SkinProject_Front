5e988a71dad9ecd6dfb25a9ccc6cf90f30d2e013
DatSatoru <almarseda@gmail.com>
2025-02-14 14:56:04 +0100

    edited the card display with the sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CardProductComponent } from "./components/card-product/card-product.component";
 import { ProductsComponent } from "./components/products/products.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductsComponent, SidebarComponent],
+  imports: [ProductsComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
