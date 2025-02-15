3dfcf18fc44a9684a77f1394514bcdc75cdc3222
DatSatoru <almarseda@gmail.com>
2025-02-15 11:59:36 +0100

    created color filter component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { ProductService } from '../../services/product-services.service';
 import { PriceComponent } from "../filters/price/price.component";
+import { ColorComponent } from "../filters/color/color.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [PriceComponent],
+  imports: [PriceComponent, ColorComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
