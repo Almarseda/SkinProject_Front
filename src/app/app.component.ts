5dc60b64ded0090ce3d37689d0cf840cded4e5ba
DatSatoru <almarseda@gmail.com>
2025-02-15 18:52:54 +0100

    made some changes on tailwind and CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ProductsComponent } from "./components/products/products.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductsComponent, SidebarComponent],
+  imports: [ProductsComponent, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
