7f27cce0d6e2fa1dc48830ba183dbb341bd25208
DatSatoru <almarseda@gmail.com>
2025-02-16 16:26:33 +0100

    added routerlink to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SearchComponent } from "../filters/search/search.component";
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
